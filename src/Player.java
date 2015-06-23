48b979d3e866c64b638a998eb122c608f95d881c
student <student@130.253.175.123>
2015-06-23 15:26:53 -0600

    player commit

--- src/Player.java
+++ src/Player.java
@@ -4,7 +4,18 @@ public class Player
 
 	public static void main(String[] args) 
 	{
-		int playerHealth = 100
+		
+		int playerHealth = 100;
+		int playerLevel = 1;
+		
+	}
+	public static void print(String stuff)
+	{
+		System.out.println(stuff);
+	}
+	public static void pause()
+	{
+		 Thread.sleep(11, nanos);
 	}
 
 }
