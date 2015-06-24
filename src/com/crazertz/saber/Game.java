3dd9af0bab8e193e346ebf1ab7dc8c306e88c997
student <student@130.253.175.123>
2015-06-24 11:01:05 -0600

    Worked in display and game.java on graphics.

--- src/com/crazertz/saber/Game.java
+++ src/com/crazertz/saber/Game.java
@@ -1,4 +1,10 @@
 package com.crazertz.saber;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 import com.crazertz.saber.display.Display;
 
@@ -7,19 +13,73 @@ public class Game implements Runnable
 	private Display display;
 	private int width;
 	private int height;
+	public String title;
+	
+	private BufferStrategy bs;
+	private Graphics g;
+	
+	private boolean running = false;
+	private Thread thread;
+	
 	
 	public Game(String title, int width, int height)
 	{
+		
 		this.width = width;
 		this.height = height;
-		
+		this.title = title;
+	}
+	
+	private void init()
+	{
 		display = new Display(title, width, height);
 	}
-
+	
+	private void tick()
+	{
+		
+	}
+	
+	private void render()
+	{
+		bs = display.getCanvas().getBufferStrategy();
+		
+	}
+	
 	@Override
 	public void run() 
 	{
+		init();
+		while(running)
+		{
+			tick();
+			render();
+		}
 		
+		stop();
+	}
+	
+	public synchronized void start()
+	{
+		if(running)
+			return;
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	public synchronized void stop()
+	{
+		if(running)
+			return;
+		running = false;
+		try
+		{
+			thread.join();
+		} catch(InterruptedException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 }
