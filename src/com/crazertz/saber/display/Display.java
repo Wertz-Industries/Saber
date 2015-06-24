d884daab2f0b2d0838003b9c49f13885afd72484
Christoph Wertz <christoph.wertz@gmail.com>
2015-06-23 22:00:57 -0600

    Added canvas to frame.

--- src/com/crazertz/saber/display/Display.java
+++ src/com/crazertz/saber/display/Display.java
@@ -1,10 +1,14 @@
 package com.crazertz.saber.display;
 
+import java.awt.Canvas;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class Display 
 {
 	private JFrame frame;
+	private Canvas canvas;
 	
 	private String title;
 	private int width;
@@ -29,5 +33,13 @@ public class Display
 		frame.setLocationRelativeTo(null);
 		
 		frame.setVisible(true);
+		
+		canvas = new Canvas();
+		canvas.setPreferredSize(new Dimension(width, height));
+		canvas.setMinimumSize(new Dimension(width, height));
+		canvas.setMaximumSize(new Dimension(width, height));
+		
+		frame.add(canvas);
+		frame.pack();
 	}
 }
