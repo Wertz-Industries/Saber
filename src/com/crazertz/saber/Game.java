13ac7f2e3839ee1ed933cb5a083fc167fe0a5001
Christoph Wertz <christoph.wertz@gmail.com>
2015-06-24 21:55:00 -0600

    Add images and sprite sheets.

--- src/com/crazertz/saber/Game.java
+++ src/com/crazertz/saber/Game.java
@@ -2,12 +2,14 @@ package com.crazertz.saber;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 
 import com.crazertz.saber.display.Display;
+import com.crazertz.saber.gfx.ImageLoader;
 
 public class Game implements Runnable
 {
@@ -22,6 +24,8 @@ public class Game implements Runnable
 	private boolean running = false;
 	private Thread thread;
 	
+	private BufferedImage ship, marine;
+	
 	public Game(String title, int width, int height)
 	{
 		
@@ -33,6 +37,9 @@ public class Game implements Runnable
 	private void init()
 	{
 		display = new Display(title, width, height);
+		ship = ImageLoader.loadImage("/textures/fighter-000.png");
+		
+		marine = ImageLoader.loadImage("/textures/Space-Marine-Game-Sprite-Sheet.jpg");
 	}
 	
 	private void tick()
@@ -55,6 +62,8 @@ public class Game implements Runnable
 		g.clearRect(0, 0, width, height);
 		// Draw here!
 
+		g.drawImage(ship, 50, 50, null);
+		g.drawImage(marine, 50, 200, null);
 		
 		// End drawing!
 		bs.show();
